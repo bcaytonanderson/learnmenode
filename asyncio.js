cc3d3f6380e4b50d37789542926c6f12fc284124
Bradley Anderson <bcaytonanderson@gmail.com>
2016-09-26 16:51:51 -0500

    Lesson 4: Async I/O; first attempt. Learning how to do things the Node.js way- asynchronously. It is very similar to the previous lesson, but requires a restructuring of the code to comply with Node's async nature.

---
+++ asyncio.js
@@ -0,0 +1,44 @@
+//Lesson 4: Write a program that uses a single asynchronus filesystem operation to read a file
+//and print the number of newlines it contains.
+
+//Very similar to previous lesson, but now in the "Node.js way"- asynchronusly.
+
+// function callback (err, data) {
+
+// };
+// //This is the idiomatic Node.js callback function format. You can check for errors by checking
+// whether the first argument comes back truthy- if it does, then the second argument will be a 
+// Buffer object.
+
+// =============================================================
+//First attempt.
+
+var fs = require('fs');
+
+var lines = 0;
+
+function linecount (callback) {
+	fs.readFile(process.argv[2], 'utf8', function doneReading(err, fileContents) {
+		lines = fileContents.split('\n').length -1;
+		callback();
+	})
+};
+
+function logLineCount() {
+	console.log(lines);
+};
+
+linecount(logLineCount);
+
+//The official solution is as follows:
+
+// var fs = require('fs')  
+// var file = process.argv[2]  
+       
+// fs.readFile(file, function (err, contents) {  
+//     // fs.readFile(file, 'utf8', callback) can also be used  
+//     var lines = contents.toString().split('\n').length - 1  
+//     console.log(lines)  
+// })  
+
+// My solution was far more verbose. First they required fs, then set a variable file which would be the path to the file the program would read. Next, they call fs.readFile(), supplying it with file as an argument, and writing the callback as the second argument. They then create a variable, lines, and perform the necessary functions to return the number of new lines. Then, within the callback, they console.log the return.
